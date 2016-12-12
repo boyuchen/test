103a948f612caaba2632061720e85c72869e6118
boyuchen <450163648@qq.com>
2016-12-12 09:44:59 +0800

    Create index.cs

---
+++ index.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qisda.DAL;
+using Qisda.Model;
+
+namespace Qisda.Service
+{
+    public class QisdaService : IQisdaService
+    {
+        public void Add(HomeLineH h)
+        {
+            using(QisdaContext context = new QisdaContext())
+            {
+                context.HomeLineHs.Add(h);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// 获取首件换线历史
+        /// </summary>
+        /// <param name="id">线别</param>
+        /// <returns></returns>
+        public HomeLineH GetHomeLineQuery(int id, bool realtime)
+        {
+
+            using (var dbContext = new QisdaContext())
+            {
+                HomeLineH data = new HomeLineH();
+                if (!realtime)
+                {
+                    var hline = dbContext.HomeLineHs.Where(h => h.LineType == id).FirstOrDefault();
+                    data = hline;
+                    data.Assistant = hline.Assistant;
+                    data.Technician = hline.Technician;
+                }
+                return data;
+            }           
+        }
+    }
+}
